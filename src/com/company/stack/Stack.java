67f72a683cf2c93ba660cdeac40b0fa9e351731a
Levon_Aloyan <>
2021-08-20 18:56:46 +0400

    Initial commit

---
+++ src/com/company/stack/Stack.java
@@ -0,0 +1,8 @@
+package com.company.stack;
+
+public interface Stack {
+
+    int pop();
+
+    void push(int element);
+}
---
+++ src/com/company/stack/StackImpl.java
@@ -0,0 +1,13 @@
+package com.company.stack;
+
+public class StackImpl implements Stack{
+    @Override
+    public int pop() {
+        return 0;
+    }
+
+    @Override
+    public void push(int element) {
+
+    }
+}
